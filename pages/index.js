0787c06d3a8be68700eb573e00df1036f5e08b4e
Siow Ming Kang <62229606+MingKangS@users.noreply.github.com>
2022-05-18 12:49:55 +0800

    Add NewsCard

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,29 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import React, { useState } from 'react';
+import NewsCard from '../components/NewsCard';
 
 
 export default function Home() {
 	const [searchQuery, setSearchQuery] = useState("")
+	const [searchResults, setSearchResults] = useState([])
 	
   return (
     <div className={styles.container}>
       <div className="header">
 				<h1>Hacker News</h1>
 			</div>
-			<div>
+			<div className="searchBarContainer">
 				<input 
 					className="searchBar"
 					placeholder="Search for your Hacker News here"
 					onChange={(e) => setSearchQuery(e.target.value)}
 				/>
+			</div>
+			<div className="newsCardContainer">
+				{searchResults.map((n, idx) => (
+          <NewsCard/>
+        ))}
 			</div>
     </div>
   )
