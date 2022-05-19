0365c2ab464311bd633a30a87fd1e26a12c42a43
Siow Ming Kang <62229606+MingKangS@users.noreply.github.com>
2022-05-19 23:28:59 +0800

    Add NewsCard styles

--- components/NewsCard.js
+++ components/NewsCard.js
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import AvatarGenerator from 'react-avatar-generator';
+import styles from '../styles/NewsCard.module.css'
 
-const NewsCard = ({ newsItem, styles }) => {
+const NewsCard = ({ newsItem, indexStyles }) => {
 	let text = "once upon a time, \t there was a man \t who had a house." ;
 
 	const formatHighlightResult = (val) => {
@@ -16,15 +17,26 @@ const NewsCard = ({ newsItem, styles }) => {
 	return ( 
 		<Link href={"/" + newsItem.objectID}>
       <div className={styles.newsCard}>
-				<AvatarGenerator
-					colors={[randomHex(), randomHex(), randomHex()]}
-					backgroundColor={"#fff"}
-					width={40}
-					height={40}
-				/>
-        <p className={styles.title}><em>{ formatHighlightResult(newsItem._highlightResult.author.value) }</em></p>
-        <p className={styles.title}>{ formatHighlightResult(newsItem._highlightResult.title.value) }</p>
+				<div className={styles.cardHeaderDiv}>
+					<AvatarGenerator
+						colors={[randomHex(), randomHex(), randomHex()]}
+						backgroundColor={"#fff"}
+						width={30}
+						height={30}
+					/>
+					<p className={styles.authorName}><em>{ formatHighlightResult(newsItem._highlightResult.author.value) }</em></p>
+					<p className={styles.createdAt}>{ newsItem.created_at.slice(0,10) }</p>
+				</div>
+				
         
+        <p className={styles.title}>{ formatHighlightResult(newsItem._highlightResult.title.value) }</p>
+				{
+					newsItem._highlightResult.url &&
+					<p className={styles.title}>{ formatHighlightResult(newsItem._highlightResult.url.value) }</p>
+				}
+				
+				<p className={styles.title}>{ newsItem.points } points</p>
+				<p className={styles.title}>{ newsItem.num_comments } comments</p>
       </div>
     </Link>
 	 );
